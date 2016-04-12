9fce75b0a98be81a1e55d8852cfc0855525b4fef
Joshua Zweig <jmz2135@columbia.edu>
2016-04-12 00:33:03 -0400

    Add scoring functions templates

---
+++ score.py
@@ -0,0 +1,13 @@
+#Normlaize all scores to 0-100
+
+def score_res_nat_park(res_dic, nat_park_dic):
+  #compute score
+
+def score_res_water(res_dic, water_dic):
+  #copmute score
+
+def score_res_fault(res_dic, fault_dic):
+  #compute score
+
+def score_res_water_source(res_dic, water_source_dic):
+  #compute score
