419947849661b97856ac2e76b6df157f4bba8158
Joshua Zweig <jmz2135@columbia.edu>
2016-04-13 23:23:14 -0400

    Revise scorer functions

--- score.py
+++ score.py
@@ -5,9 +5,15 @@ def score_res_nat_park(res_dic, nat_park_dic):
 
 def score_res_water(res_dic, water_dic):
   #copmute score
+  dist = sqrt(pow((res_dic['Long'] - water_dic['Long']), 2) + pow((res_dic['Lat'] - water_dic['Lat']), 2))
+  water_importance = water_dic['Score']
+
+  return float(water_importance) / float(dist * dist) #reflects inverse relationship between distance and source
 
 def score_res_fault(res_dic, fault_dic):
   #compute score
+  # dist = sqrt(pow((res_dic['Long'] - fault_dic['Long']), 2) + pow((res_dic['Lat'] - fault_dic['Lat']), 2))
+  # fault_risk_score = fault_dic['Score']
 
-def score_res_water_source(res_dic, water_source_dic):
+def score_population(res_dic, population_dic):
   #compute score
